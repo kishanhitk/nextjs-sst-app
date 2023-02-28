eb9c7eba5d60d756f9a4f581ba1b3d22fa0ba303
kishanhitk <kishansharma1231@gmail.com>
2023-02-28 17:10:50 +0530

    feat: add revalidation

--- packages/frontend/src/app/todos/page.tsx
+++ packages/frontend/src/app/todos/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 const Index = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+    next: {
+      revalidate: 1000,
+    },
+  });
   const data = await res.json();
   console.log(data);
   return (
     <div>
       App dir
-      {data.map((item) => (
+      {data.map((item: any) => (
         <div key={item.id}>{item.title}</div>
       ))}
     </div>
